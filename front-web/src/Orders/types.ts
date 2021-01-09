7f25e142e2e29fd978c3e151ba411789edd82417
Alan Santos <alanssantos32@gmail.com>
2021-01-09 14:30:16 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -6,8 +6,16 @@ export type Product = {
     imageUri: string;
 }
 
-export type OrderLOcationData = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayLoad = {
+    products: ProductId[];
+} & OrderLocationData;
